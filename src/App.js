32ee3a5a3abd9cb86155851e90f73dde5a769575
Fernando Cuatzo <cuatzo.fernando@gmail.com>
2022-11-23 07:04:22 -0600

    carrito de compras realizado con clases de React, basico

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import Productos from './components/Productos'
+import Layout from './components/Layout'
+import Title from './components/Title'
+import Navbar from './components/Navbar'
+class App extends Component {
+  state = {
+    productos: [
+      { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
+      { name: 'Arbejas', price: 500, img: '/productos/arbejas.jpg'},
+      { name: 'Lechuga', price: 2500, img: '/productos/lechuga.jpg'}
+    ],
+    carro: [
+
+    ],
+    esCarroVisible: false
+  }
+
+  agregarAlCarrito = (producto) => {
+    const { carro } = this.state
+    const findCarro = carro.find((f) => f.name === producto.name)
+    if (findCarro) {
+      const newCarro = carro.map(x => x.name === producto.name 
+        ? ({ 
+          ...x,
+          cantidad: x.cantidad + 1
+        })
+        : x)
+        return this.setState({ carro: newCarro })
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1
+      })
+    })
+  }
+
+  mostrarCarro = () => {
+    if (!this.state.carro.length) {
+      return
+    }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible })
+  }
+
+  render() {
+    const { esCarroVisible } = this.state
+    return (
+      <div>
+        <Navbar 
+          carro={this.state.carro} 
+          esCarroVisible={esCarroVisible} 
+          mostrarCarro={this.mostrarCarro} 
+        />
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarrito={(this.agregarAlCarrito)}
+            productos={this.state.productos}   
+          />    
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default App
